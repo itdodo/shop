ae82dafdcb7ec6aea8cf6cdfa542c13f69f17ef8
itdodo <itdodo@163.com>
2015-01-21 17:30:18 +0800

    引入FlatUI，并添加后台登录页面

---
+++ Shop.View/Controllers/Admin/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers.Admin
+{
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// Bootstrap
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// FlatUI
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
